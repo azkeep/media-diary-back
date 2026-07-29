9e5cd9381863e73dae2c55400599f8bd82196697
Oleksandr-Braiko <azkeep@gmail.com>
2026-07-29 21:11:30 +0300

    chore(ISS-103): change file organization in handler package

---
+++ handler/middleware.go
@@ -0,0 +1,17 @@
+package handler
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler, allowedOrigin string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
